5c363d8451b987b78e54a0cd2db4be10c2371b0d
Hicham Guendouz <Hicham.guendouz77500@gmail.Com>
2026-03-07 17:36:44 +0100

    feat: configure Next.js for GitHub Pages deployment

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+    basePath: '/Portfolio',
   reactStrictMode: true,
   images: {
+      unoptimized: true,
     formats: ['image/avif', 'image/webp'],
     domains: ['images.ctfassets.net', 'res.cloudinary.com'],
   },
